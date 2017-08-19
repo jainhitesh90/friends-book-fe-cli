cecedc9604c1ba1844163806ca6f671b05b6b838
Hitesh Jain <jainhitesh90@gmail.com>
2017-08-20 01:34:54 +0530

    env file comments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,14 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyCagNDTOSBFIYsFL-W0mzqvOGhk9JCergw',
-    authDomain: 'musicfeeds2017-44f58.firebaseapp.com',
-    databaseURL: 'https://musicfeeds2017-44f58.firebaseio.com',
-    projectId: 'musicfeeds2017-44f58',
-    storageBucket: 'fir-project-486ea.appspot.com',
-    messagingSenderId: '209914723434'
-  }
+  production: false
+  // ,
+  // firebase: {
+  //   apiKey: 'AIzaSyCagNDTOSBFIYsFL-W0mzqvOGhk9JCergw',
+  //   authDomain: 'musicfeeds2017-44f58.firebaseapp.com',
+  //   databaseURL: 'https://musicfeeds2017-44f58.firebaseio.com',
+  //   projectId: 'musicfeeds2017-44f58',
+  //   storageBucket: 'fir-project-486ea.appspot.com',
+  //   messagingSenderId: '209914723434'
+  // }
 };
